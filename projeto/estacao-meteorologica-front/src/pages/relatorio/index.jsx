7a67007859936e3aa740505c17d9f8fbd8175ec5
davizoladosmoveisplanejados <davi.sousa17@aluno.senai.br>
2026-04-27 10:39:09 -0300

    Atualização Estação Meteorológica

---
+++ projeto/estacao-meteorologica-front/src/pages/relatorio/index.jsx
@@ -0,0 +1,52 @@
+import Header from "../../components/header";
+
+export default function Relatorio(){
+
+    const leituras = [
+        {horario: "12:00", qualidadeAr: "Boa", iqa: 42, temperatura: '26°C', umidade: '67%'},
+        {horario: "13:00", qualidadeAr: "Boa", iqa: 45, temperatura: '27°C', umidade: '69%'},
+        {horario: "14:00", qualidadeAr: "Boa", iqa: 47, temperatura: '28°C', umidade: '64%'},
+        {horario: "15:00", qualidadeAr: "Média", iqa: 39, temperatura: '29°C', umidade: '63%'}
+    ];
+
+    return(
+        <>
+        <Header />
+        <div className="container">
+            <h3>Relatório Estação Meteorológica</h3>
+            <p>Monitoramento da temperatura e umidade em tempo real</p>
+
+            <section className="graficos">
+                {/* colocar graficos  */}
+            </section>
+
+            <section className="tabela-leituras">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Horário</th>
+                            <th>Qualidade Ar</th>
+                            <th>IQA</th>
+                            <th>Temperatura</th>
+                            <th>Umidade</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            leituras.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.horario}</td>
+                                    <td>{item.qualidadeAr}</td>
+                                    <td>{item.iqa}</td>
+                                    <td>{item.temperatura}</td>
+                                    <td>{item.umidade}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </section>
+        </div>
+        </>
+    )
+}
--- projeto/estacao-meteorologica-front/src/routes/index.jsx
+++ projeto/estacao-meteorologica-front/src/routes/index.jsx
@@ -4,7 +4,7 @@ import Login from '../pages/login';
 import Registro from '../pages/registro';
 import Dashboard from '../pages/dashboard';
 import Cadastro from '../pages/cadastro';
-// import Relatorio from '../pages/relatorio';
+import Relatorio from '../pages/relatorio';
 // import NotFound from  '../pages/notfound';
 
 export default function Rotas(){
@@ -14,7 +14,7 @@ export default function Rotas(){
             <Route path="/registro" element={<Registro />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/cadastro" element={<Cadastro />} />
-            {/* <Route path="/relatorio" element={<Relatorio />} /> */}
+            <Route path="/relatorio" element={<Relatorio />} />
             {/* <Route path="*" element={<NotFound />} />   */}
         </Routes>
     )
